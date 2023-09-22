369ad8c59c3ce745a9f0f54639673ee345469b6e
grace <gracechandra75@gmail.com>
2023-09-22 22:56:59 +0530

    collatz_sequence

--- practice_problems/Collatz_sequence.java
+++ practice_problems/Collatz_sequence.java
@@ -1,31 +1,34 @@
-import java.math.BigInteger;
-import java.util.Scanner;
-
 public class Collatz_sequence {
 
-	static void sequence_generate(BigInteger n) {
-		
-		System.out.print(n);
-		
-		if(!n.equals(BigInteger.valueOf(1))) {
-			
-			if(n.mod(BigInteger.valueOf(2)).equals(BigInteger.valueOf(0))) {
-				n=n.divide(BigInteger.valueOf(2));
-			}else {
-				n=(BigInteger.valueOf(3).multiply(n)).add(BigInteger.valueOf(1));
+	static void sequence_generate() {
+
+		long n = 1000000;
+		long max = 0;
+		long value = 0;
+
+		while (n > 0) {
+			long count = 1;
+			long i = n;
+			while (i > 1) {
+				if (i % 2 == 0) {
+					i = i / 2;
+				} else {
+					i = (3 * i) + 1;
+				}
+				count++;
 			}
-			System.out.print("-->");
-		    sequence_generate(n);
+			if (count > max) {
+				max = count;
+				value = n;
+			}
+			n--;
 		}
+
+		System.out.println("MAXIMUM COUNT :" + max);
+		System.out.println("NUMBER IS :" + value);
 	}
+
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		
-		System.out.print("ENTER THE NUMBER : ");
-		String s=sc.next();
-		
-		BigInteger n = new BigInteger(s);
-		
-		sequence_generate(n);
+		sequence_generate();
 	}
 }

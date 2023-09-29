edcaf64cc12dedb8015185716b49ec502418d8d8
grace <gracechandra75@gmail.com>
2023-09-29 16:27:50 +0530

    changes

--- Mysql/team_league.sql
+++ Mysql/team_league.sql
@@ -1,13 +1,42 @@
 create database sports;
 use sports;
-create table matches(Match_id varchar(10),Match_name varchar(10),Dates date,Start_time time,End_time time,Winner_team varchar(20));
-create table players(player_id varchar(10) primary key,First_name varchar(10),Last_name varchar(10),Team_id varchar(10),FOREIGN KEY (Team_id) references teams(Team_id));
+drop table matches;
+create table matches(Match_id varchar(10) primary key,
+                     Match_name varchar(10),
+                     Dates date,
+                     Start_time time,
+                     End_time time,
+                     Winner_team varchar(20));
+create table players(player_id varchar(10) primary key,
+                     First_name varchar(10),
+                     Last_name varchar(10),
+                     Team_id varchar(10),
+                     FOREIGN KEY (Team_id) references teams(Team_id));
 drop table players; 
-create table teams(Team_id varchar(10) primary key,Team_name varchar(20),Total_members int);
+create table teams(Team_id varchar(10) primary key,
+                   Team_name varchar(20),
+                   Total_members int);
 drop table teams; 
 alter table teams modify Team_id varchar(10) primary key;
-create table players_score(player_id varchar(10),Team_id varchar(10),Match_id varchar(10),Score int, FOREIGN KEY (Team_id) references teams(Team_id),FOREIGN KEY (player_id) references players(player_id));
+create table players_score(player_id varchar(10),
+                           Team_id varchar(10),
+                           Match_id varchar(10),
+                           Score int, 
+                           FOREIGN KEY (Team_id) references teams(Team_id),
+                           FOREIGN KEY (player_id) references players(player_id));
 SHOW TABLES;
 describe teams;
-create table team_score(team_id varchar(10),Match_id varchar(10),Score int, FOREIGN KEY (Team_id) references teams(Team_id));
+drop table team_score;
+create table team_score(Team_id varchar(10),
+						Match_id varchar(10),
+                        Score int,
+                        FOREIGN KEY (Team_id) references teams(Team_id),
+                        FOREIGN KEY (Match_id) references matches(Match_id));
+                        
+create table match_payed(Match_id varchar(10),
+                         Match_name varchar(20),
+                         Team_id varchar(10),
+                         Team_name varchar(20),
+                         FOREIGN KEY (Team_id) references teams(Team_id),
+                         FOREIGN KEY (Match_id) references matches(Match_id));
 drop database sports;

49da4f0410660658ded7b727c43d4c497862ced1
grace <gracechandra75@gmail.com>
2023-10-04 16:59:48 +0530

    changes

--- Mysql/final_exam.sql
+++ Mysql/final_exam.sql
@@ -1,5 +1,6 @@
 create database final_exam;
 use final_exam;
+drop database final_exam;
 
 create table course(course_id varchar(10) primary key,
                     course_name varchar(20),
@@ -10,9 +11,11 @@ describe course;
 create table room(room_id varchar(10) primary key,
                     capacity int,
                     building varchar(10));
-                    
+                  
+drop table section;
+				
 create table section(section_no varchar(10),
-                    course_id varchar(10),
+                    course_id varchar(30),          -- using int instead of varchar error , changing size no error ,
                     enrollment int,
                     foreign key (course_id) references course(course_id));
                     
